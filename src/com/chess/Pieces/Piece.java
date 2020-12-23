047d963c70025ddcf96278d492496b7ff0d95db2
usernamenna <56196315+usernamenna@users.noreply.github.com>
2020-12-23 07:59:26 +0200

    Update Piece.java

--- src/com/chess/Pieces/Piece.java
+++ src/com/chess/Pieces/Piece.java
@@ -1,8 +1,10 @@
 package com.chess.Pieces;
 
+import com.chess.Game.*;
+
 public abstract class Piece {
-    private final int black = 0;
-    private final int white = 1;
+    private static final int black = 0;
+    private static final int white = 1;
     private final int color; // 0 for black, 1 for white
     private int x;
     private int y;// Position
@@ -21,19 +23,107 @@ public abstract class Piece {
     public int getY() {
         return y;
     }
-
+    public int getColor()
+    {
+        return color;
+    }
     public boolean isFirstMove() {
         return isFirstMove;
     }
-
-    // TODO add isValidMove, move body
-
     public abstract boolean isValidMove(int xPos, int yPos);
-    public abstract void move(int xPos, int yPos);
-
-    public void removePiece(Piece piece) //
-    {
-        this.x = -1;
-        this.y = -1;
+    public void move(Spot from, Spot to){
+        from.removePiece();
+        to.insertPiece(this);
+        x = to.getX();
+        y = to.getY();
     }
+    // generates possible moves for a piece
+    protected abstract void possibleMoves();
+    protected boolean canMoveStraight(int toX, int toY,Board board)
+    {
+        if(toX==x && toY == y)
+        {
+            return false;
+        }
+        if(toX == x) // moving on y
+        {
+            int small = Math.min(y, toY);
+            int big = Math.max(y, toY);
+            // check if there's an occupied spot on the way
+            for(int i = small;i < big; i++)
+            {
+                if(!board.getSpot(x, i).isEmpty()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        if(toY == y) // moving on x
+        {
+            int small = Math.min(x, toX);
+            int big = Math.max(x, toX);
+            // check if there's an occupied spot on the way
+            for(int i = small;i < big; i++)
+            {
+                if(!board.getSpot(i, y).isEmpty()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+    protected boolean canMoveDiagonal(int toX, int toY, Board board)
+    {
+        if(x == toX && y == toY)
+        {
+            return false;
+        }
+        // right diagonal
+        if((toX > x && toY > y) || (toX < x && toY < y))
+        {
+            // check if a spot is occupied in the way
+            for(int i = x; i < Math.abs(toX - x); i++)
+            {
+                if(toX > x && toY > y)
+                {
+                    if(!board.getSpot(x+i, y+i).isEmpty())
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                   if(!board.getSpot(x-i, y-i).isEmpty())
+                   {
+                       return false;
+                   }
+                }
+            }
+        }
+        // left diagonal
+        else if((toX>x && toY < y)|| (toX < x && toY > y))
+        {
+            // check if a spot is occupied in the way
+            for(int i = x; i < Math.abs(toX - x); i++)
+            {
+                if(toX > x && toY < y)
+                {
+                    if(!board.getSpot(x+i, y-i).isEmpty())
+                    {
+                        return false;
+                    }
+                }
+                else if(toX < x && toY > y)
+                {
+                    if(!board.getSpot(x-i, y+i).isEmpty())
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
 }
