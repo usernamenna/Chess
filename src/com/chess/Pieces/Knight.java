bf3a074c32f8d2a69d46d5b43e06a6fda21f364b
usernamenna <56196315+usernamenna@users.noreply.github.com>
2020-12-24 14:41:11 +0200

    Add files via upload

---
+++ src/com/chess/Pieces/Knight.java
@@ -0,0 +1,44 @@
+package com.chess.Pieces;
+
+import com.chess.Game.Board;
+import com.chess.Game.Spot;
+
+import java.util.ArrayList;
+
+public class Knight extends Piece{
+
+    ArrayList<Spot> moves = new ArrayList<>();
+    public Knight(int x, int y, int color, boolean isFirstMove) {
+        super(x, y, color, isFirstMove);
+    }
+
+    @Override
+    public boolean isValidMove(int xPos, int yPos) {
+        // Knight can move two squares and one square in any direction
+        if (Math.abs(xPos - getX()) == 1 && Math.abs(yPos - getY()) == 2)
+            return true;
+        else if (Math.abs(xPos - getX()) == 2 && Math.abs(yPos - getY()) == 1)
+            return true;
+        return false;
+    }
+
+    @Override
+    public ArrayList<Spot> possibleMoves(Board board) {
+        moves.clear();
+        // Knight can move two squares and one square in any direction
+        int[] X = {1, 1, -1, -1, 2, 2, -2, -2};
+        int[] Y = {2, -2, 2, -2, 1, -1, 1, -1};
+        for(int i = 0 ; i < 8 ; i++)
+        {
+            int newX = getX()+X[i];
+            int newY = getY()+Y[i];
+            Spot spot = board.getSpot(newX, newY);
+            // check if spot is valid and empty OR contains a piece of the opposite color
+            if(board.isValidPosition(newX, newY) || (spot.getPiece()!= null && spot.getPiece().getColor()!= this.getColor()))
+            {
+                moves.add(board.getSpot(newX, newY));
+            }
+        }
+        return moves;
+    }
+}
