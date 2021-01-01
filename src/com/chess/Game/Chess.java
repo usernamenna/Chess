8bf6b59342d0001d1c1e7e8cf66983ee46a9d1b3
norhan333 <62456227+norhan333@users.noreply.github.com>
2021-01-01 06:51:31 +0200

    Update Chess.java

--- src/com/chess/Game/Chess.java
+++ src/com/chess/Game/Chess.java
@@ -1,8 +1,9 @@
 package com.chess.Game;
 
-public class Main {
+public class Chess {
     public static void main(String[] args)
     {
         Game game = new Game();
+        game.setVisible(true);
     }
 }
