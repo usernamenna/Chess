f7ed8a1045b22bbcf94110405ba019ecd57cfedb
norhan333 <62456227+norhan333@users.noreply.github.com>
2021-01-01 05:34:42 +0200

    Update Game

--- src/com/chess/Game/Game.java
+++ src/com/chess/Game/Game.java
@@ -2,10 +2,11 @@ package com.chess.Game;
 
 import javax.swing.*;
 import java.awt.*;
+import com.chess.Pieces.*;
 
 public class Game extends JFrame {
     Board board = new Board();
-    Game()
+    public Game()
     {
         super();
         setTitle("Chess");
@@ -16,13 +17,104 @@ public class Game extends JFrame {
         {
             for(int j = 0 ; j < 8 ; j++)
             {
-                Spot spot = board.getSpot(i, j);
-                add(spot);
-                spot.revalidate();
-                spot.repaint();
+              Spot spot = board.getSpot(i, j);
+              add(spot);
+              spot.getVisibleRect();
+              spot.invalidate();
             }
         }
+        Setup();
         setLayout(new GridLayout(8,8, 0, 0));
         setVisible(true);
+        validate(); 
     }
+    
+    // setting up pices
+    public void Setup ()
+    {
+        
+         for(int i = 0 ; i < 2 ; i++)
+        {
+            for(int j = 0 ; j < 8 ; j++)
+            {
+                if (i ==0)
+                {
+                    if ( j == 0 || j ==7 )
+                    {
+                        Piece rook = new Rook(i,j,0);
+                        board.getSpot(i, j).insertPiece(rook);
+                    }
+                    else if ( j == 1 || j ==6 )
+                    {
+                        Piece knight = new Knight(i,j,0);
+                        board.getSpot(i, j).insertPiece(knight);
+                    }
+                    else if ( j == 2 || j == 5)
+                    {
+                        Piece bishop = new Bishop(i,j,0);
+                        board.getSpot(i, j).insertPiece(bishop);
+                    }
+                    else if ( j == 3)
+                    {
+                        Piece king = new King(i,j,0);
+                        board.getSpot(i, j).insertPiece(king);
+                    }
+                    else if ( j == 4)
+                    {
+                        Piece queen = new Queen(i,j,0);
+                        board.getSpot(i, j).insertPiece(queen);
+                    }
+                   
+                }
+                else if ( i == 1)
+                {
+                    Piece pawn = new Pawn(i,j,0);
+                    board.getSpot(i, j).insertPiece(pawn);
+                }
+            }
+        }
+        
+        
+        for(int i = 7 ; i >=6 ; i--)
+        {
+            for(int j = 0 ; j < 8 ; j++)
+            {
+                if (i ==7)
+                {
+                    if ( j == 0 || j ==7 )
+                    {
+                        Piece rook = new Rook(i,j,1);
+                        board.getSpot(i, j).insertPiece(rook);
+                    }
+                    else if ( j == 1 || j ==6 )
+                    {
+                        Piece knight = new Knight(i,j,1);
+                        board.getSpot(i, j).insertPiece(knight);
+                    }
+                    else if ( j == 2 || j == 5)
+                    {
+                        Piece bishop = new Bishop(i,j,1);
+                        board.getSpot(i, j).insertPiece(bishop);
+                    }
+                    else if ( j == 3)
+                    {
+                        Piece king = new King(i,j,1);
+                        board.getSpot(i, j).insertPiece(king);
+                    }
+                    else if ( j == 4)
+                    {
+                        Piece queen = new Queen(i,j,1);
+                        board.getSpot(i, j).insertPiece(queen);
+                    }
+                   
+                }
+                else if ( i == 6)
+                {
+                    Piece pawn = new Pawn(i,j,1);
+                    board.getSpot(i, j).insertPiece(pawn);
+                }
+            }
+        }
+    }
+    
 }
