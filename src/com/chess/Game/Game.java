eba1d54a242a9ae809748741efe52ce96970cf0e
norhan333 <62456227+norhan333@users.noreply.github.com>
2021-01-03 15:51:30 +0200

    Update Game.java

--- src/com/chess/Game/Game.java
+++ src/com/chess/Game/Game.java
@@ -3,8 +3,12 @@ package com.chess.Game;
 import javax.swing.*;
 import java.awt.*;
 import com.chess.Pieces.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class Game extends JFrame {
+
+public class Game extends JFrame implements ActionListener {
     Board board = new Board();
     public Game()
     {
@@ -13,7 +17,6 @@ public class Game extends JFrame {
         setSize(600, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
-        setLayout(new GridLayout(8,8, 0, 0));
         for(int i = 0 ; i < 8 ; i++)
         {
             for(int j = 0 ; j < 8 ; j++)
@@ -117,6 +120,43 @@ public class Game extends JFrame {
                 }
             }
         }
+        
+        //  adding mouse listener
+          for(int i = 0 ; i < 8 ; i++)
+        {
+            for(int j = 0 ; j < 8 ; j++)
+            {
+               board.getSpot(i, j).addActionListener(this);
+            }
+        }
     }
-    
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+            Object source = ae.getSource();
+           
+           if (source instanceof Spot) {
+                Spot button = (Spot)source;
+              //  button.setBackground(Color.yellow);
+           if(!button.isEmpty())
+           {
+               ArrayList<Spot>pet = button.getPiece().possibleMoves(board);
+               
+               for(Spot i : pet)
+               {
+                   i.setBackground(Color.yellow);
+               }
+           }
+           }  
+//                button.getPiece();
+
+             // System.out.println(pet.get(0));
+           
+//
+//        for (int i = 0 ; i < pet.lastIndexOf(pet); i++)
+//           {
+//                pet.get(i).setBackground(Color.yellow);
+//           }
+           
+    }
 }
